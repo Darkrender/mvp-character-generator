3b4b56b0ff83248c439af9ad019294f89d67ccfc
Jimmie Gisclair <jimmiegisclair@att.net>
2017-01-23 15:26:55 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ var path = require('path');
 
 var app = express();
 var port = process.env.PORT || 8000;
-app.use(express.static('./../client'));
+app.use(express.static('./client'));
+
+app.get('/', function(req, res) {
+  res.send();
+});
 
 app.listen(port, function() {
   console.log('Node app is running on port ' + port);
