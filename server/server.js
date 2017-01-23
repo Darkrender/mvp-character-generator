8fb1a2cf8cf74b3434ae64d798afd50d90c0956a
Jimmie Gisclair <jimmiegisclair@att.net>
2017-01-23 14:03:28 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 
 var app = express();
+var port = process.env.PORT || 8000;
 
 app.get('/', function(request, response) {
   response.render('client/index.html');
 });
 
-app.listen(8000, function() {
-  console.log('Node app is running on port 8000');
+app.listen(port, function() {
+  console.log('Node app is running on port ' + port);
 });
