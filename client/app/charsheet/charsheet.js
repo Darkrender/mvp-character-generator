597d50c1957bcd23854ad7020698214c3b4726ae
Jimmie Gisclair <jimmiegisclair@att.net>
2017-01-23 23:16:21 -0800

    Add ability to post data to the database

--- client/app/charsheet/charsheet.js
+++ client/app/charsheet/charsheet.js
@@ -1,14 +1,22 @@
 angular.module('RPG-Character-Generator')
-  .controller('csFormCtrl', function($scope) {
+  .controller('csFormCtrl', function($scope, $http) {
     $scope.name = '';
-    $scope.character = [];
+    $scope.result = '';
+    $scope.character = {};
     $scope.classes = ['Druid', 'Warrior', 'Paladin', 'Knight', 'Barbarian', 'Sorceror', 'Rogue'];
     $scope.races = ['Dwarf', 'Elf', 'Human', 'Gnome'];
 
     $scope.submit = function() {
-      $scope.character.push(this.name);
-      $scope.character.push(this.selectedClass);
-      $scope.character.push(this.selectedRace);
+      $scope.character.name = this.name;
+      $scope.character.race = this.selectedRace;
+      $scope.character.class = this.selectedClass;
       $scope.name = '';
+      $scope.result = '' + $scope.character;
+
+      $http({
+        method: 'POST',
+        url: '/characterlist',
+        data: JSON.stringify($scope.character)
+      })
     };
   });
--- server/server.js
+++ server/server.js
@@ -1,12 +1,53 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var app = express();
 var port = process.env.PORT || 8000;
 
 mongoose.connect('mongodb://heroku_jwgf93cf:mvf9vhou2n0balvoeivp6319et@ds127439.mlab.com:27439/heroku_jwgf93cf');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function() {
+  //We're connected!
+  console.log('Successfully connected to database');
+});
+
+var characterSchema = mongoose.Schema({
+    name: String,
+    race: String,
+    class: String
+  });
+var Character = mongoose.model('Character', characterSchema);
+
 
 app.use(express.static('./client'));
+app.use(bodyParser());
+
+app.post('/characterlist', function(req, res) {
+  console.log('request: ', req.body);
+  console.log('Got the post!');
+
+  var c = new Character({
+      name: req.body.name,
+      class: req.body.class,
+      race: req.body.race
+  });
+
+  c.save(function(err) {
+      if (err)
+          throw err;
+      else
+          console.log('save user successfully...');
+  });
+});
+
+app.get('/characterlist', function(req, res) {
+  res.writeHead(200);
+  console.log('I see you\'re trying to get something...');
+
+});
 
 app.listen(port, function() {
   console.log('Node app is running on port ' + port)
