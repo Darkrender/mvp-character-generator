6e54cd6fbce10fb50b8ee5b0fa65ebae3323fa6c
Jimmie Gisclair <jimmiegisclair@att.net>
2017-02-02 21:00:20 -0800

    Remove all mongoose comments from server file

--- server/server.js
+++ server/server.js
@@ -5,25 +5,6 @@ var app = express();
 var port = process.env.PORT || 8000;
 var db = require('../database/schemas.js');
 
-// mongoose.connect('mongodb://heroku_jwgf93cf:mvf9vhou2n0balvoeivp6319et@ds127439.mlab.com:27439/heroku_jwgf93cf');
-// var db = mongoose.connection;
-
-// db.on('error', console.error.bind(console, 'connection error'));
-// db.once('open', function() {
-//   console.log('Successfully connected to database');
-// });
-
-// var characterSchema = mongoose.Schema({
-//     name: String,
-//     race: String,
-//     class: String,
-//     trait: String,
-//     flaw: String,
-//     alignment: String,
-//     notes: String
-//   });
-// var Character = mongoose.model('Character', characterSchema);
-
 app.use(express.static('./client'));
 app.use(bodyParser());
 
@@ -40,23 +21,6 @@ app.post('/characterlist', function(req, res) {
     alignment: req.body.alignment,
     notes: req.body.notes
   });
-
-  // var c = new Character({
-  //     name: req.body.name,
-  //     class: req.body.class,
-  //     race: req.body.race,
-  //     trait: req.body.trait,
-  //     flaw: req.body.flaw,
-  //     alignment: req.body.alignment,
-  //     notes: req.body.notes
-  // });
-
-  // c.save(function(err) {
-  //     if (err)
-  //         throw err;
-  //     else
-  //         console.log('save user successfully...');
-  // });
 });
 
 app.get('/characterlist', function(req, res) {
@@ -66,14 +30,6 @@ app.get('/characterlist', function(req, res) {
     .then( function(characters) {
       res.send(characters);
     });
-
-  // Character.find({}, function(err, characters) {
-  //   if (err) {
-  //     throw err;
-  //   } else {
-  //     res.send(characters);
-  //   }
-  // });
 });
 
 app.listen(port, function() {
